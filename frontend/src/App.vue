c969648b88d8ade3ece328cc6c1427c31e33a75c
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2021-04-04 01:00:31 +0700

    frontend: init vue as frontend
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <BootstrapExample/>
+    </div>
+  </div>
+</template>
+
+<script>
+import BootstrapExample from './components/BootstrapExample.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BootstrapExample
+  }
+}
+</script>
+
+<style>
+</style>
