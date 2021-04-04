c0bd71d323bc466423ab97157b2dca415c2e8f31
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2021-04-04 12:03:47 +0700

    frontend: init vue router & vue-boostrap
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,34 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <BootstrapExample/>
+  <div id="app" class="container">
+    <div id="nav">
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">Tweebs</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#" active :to="{ path: '/' }">Home</b-nav-item>
+            <b-nav-item href="#" :to="{ path: '/about' }">About</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>Account (Username)</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign In</b-dropdown-item>
+              <b-dropdown-item href="#">Register</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import BootstrapExample from './components/BootstrapExample.vue'
-
-export default {
-  name: 'App',
-  components: {
-    BootstrapExample
-  }
-}
-</script>
-
-<style>
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
