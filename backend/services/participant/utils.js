78f8ad744528fee3702f90ff9f7f8873fb2106d9
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2021-06-11 16:42:38 +0700

    add func to generate twibbon images
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

--- backend/services/participant/utils.js
+++ backend/services/participant/utils.js
@@ -1,5 +1,25 @@
-const generateTwibbon = (frame, fotoParticipant) => {
-    return
+/**
+ * 
+ * @param {Path} frame 
+ * @param {Path} fotoParticipant 
+ * @param {Path} outputPath 
+ * @returns bool
+ */
+const generateTwibbon = (frame, fotoParticipant, outputPath) => {
+
+    sharp(frame)
+    .resize(1000,1000)
+    .toBuffer({ resolveWithObject: true }) // We want it to a buffer
+    .then(({data,info}) => {
+        sharp(fotoParticipant)
+        .composite([{input:data}])
+        .toFile(outputPath, function(err) {
+           if(err == null){
+               return true
+           }
+        });
+    })
+    return false
 }
 
 module.exports = {
