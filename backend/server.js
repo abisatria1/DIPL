e42b6c0eae87ca3f5aa0a4c9fb167cb169186350
Abisatria <abi.kadek@gmail.com>
2022-01-07 11:20:06 +0800

    change server port

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const app = require("./app")
-require("dotenv").config()
+const app = require("./app");
+require("dotenv").config();
 // database
-const db = require("./config/database.config")
-const relation = require("./config/relation.config")
-const port = process.env.port
+const db = require("./config/database.config");
+const relation = require("./config/relation.config");
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   db.sync({})
-    .then(() => console.log(`Application is running on http://localhost:3000`))
-    .catch((err) => console.log(err.message))
-})
+    .then(() => console.log(`Application is running on PORT ${port}`))
+    .catch((err) => console.log(err.message));
+});
